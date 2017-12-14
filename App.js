e5578f84522689749c7da67908847590d686439b
Uilton Santos <uilton.jose@gmail.com>
2017-12-14 21:44:27 +0000

    - Implementation of Redux

--- App.js
+++ App.js
@@ -1,68 +1,41 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
+import { connect } from 'react-redux';
 
 import PlaceInput from './src/components/PlaceInput/PlaceInput';
 import PlaceList from './src/components/PlaceList/PlaceList';
 import PlaceDetail from './src/components/PlaceDetail/PlaceDetail';
+import { addPlace, deletePlace, selectPlace, deselectPlace } from './src/store/actions/index';
 
-export default class App extends React.Component {
-
-  state = {
-    places: [],
-    selectedPlace: null
-  }
+class App extends React.Component {
 
   placeAddedHandler = placeName => {
-    this.setState(prevState => {
-      return {
-        places: prevState.places.concat({
-          key: Math.random(),
-          name: placeName,
-          image: {
-            uri: "http://www.ilhabela.com.br/wp-content/uploads/2012/11/praia-do-pereque-ilhabela-fernando-tomanik-portalilhabela.jpg"
-          }
-        })
-      };
-    });
+    this.props.onAddPlace(placeName);
   }
 
   placeDeletedHandler = () => {
-    this.setState(prevState => {
-      return {
-        places: prevState.places.filter(place => {
-          return place.key !== prevState.selectedPlace.key;
-        }),
-        selectedPlace: null
-      };
-    });
+    this.props.onDeletePlace();
   }
 
   placeSelectedHandler = key => {
-    this.setState(prevState => {
-      return {
-        selectedPlace: prevState.places.find(place => place.key === key)
-      };
-    });
+    this.props.onSelectPlace(key);
   }
 
   modalClosedHandler = () => {
-    this.setState({
-      selectedPlace: null
-    });
+    this.props.onDeselectPlace();
   }
 
   render() {
-
     return (
       <View style={styles.container}>
         <PlaceDetail
-          selectedPlace={this.state.selectedPlace}
+          selectedPlace={this.props.selectedPlace}
           onItemDeleted={this.placeDeletedHandler}
           onModalClosed={this.modalClosedHandler}
         />
         <PlaceInput onPlaceAdded={this.placeAddedHandler} />
         <PlaceList
-          places={this.state.places}
+          places={this.props.places}
           onItemSelected={this.placeSelectedHandler} />
       </View>
     );
@@ -78,3 +51,21 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start',
   },
 });
+
+const mapStateToProps = state => {
+  return {
+    places: state.places.places,
+    selectedPlace: state.places.selectedPlace
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onAddPlace: (name) => dispatch(addPlace(name)),
+    onDeletePlace: () => dispatch(deletePlace()),
+    onSelectPlace: (placeKey) => dispatch(selectPlace(placeKey)),
+    onDeselectPlace: () => dispatch(deselectPlace()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/store/reducers/places.js
@@ -0,0 +1,44 @@
+import { ADD_PLACE, DELETE_PLACE, SELECT_PLACE, DESELECT_PLACE } from '../actions/actionTypes';
+
+const initialState = {
+    places: [],
+    selectedPlace: null
+};
+
+const reduce = (state = initialState, action) => {
+    switch (action.type) {
+        case ADD_PLACE:
+            return {
+                ...state,
+                places: state.places.concat({
+                    key: Math.random(),
+                    name: action.placeName,
+                    image: {
+                        uri: "https://media-cdn.tripadvisor.com/media/photo-s/07/aa/6b/fd/fernando-de-noronha.jpg"
+                    }
+                })
+            };
+        case DELETE_PLACE:
+            return {
+                ...state,
+                places: state.places.filter(place => {
+                    return place.key !== state.selectedPlace.key;
+                }),
+                selectedPlace: null
+            };
+        case SELECT_PLACE:
+            return {
+                ...state,
+                selectedPlace: state.places.find(place => place.key === action.placeKey)
+            };
+        case DESELECT_PLACE:
+            return {
+                ...state,
+                selectedPlace: null
+            };
+        default:
+            return state;
+    }
+};
+
+export default reduce;
